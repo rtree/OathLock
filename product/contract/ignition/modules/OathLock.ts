b0c578a67f6911713d3af7799f3e36d599b0105f
rtree <arakitomohiko@gmail.com>
2025-08-16 14:40:04 -0400

    ong

--- product/contract/ignition/modules/OathLock.ts
+++ product/contract/ignition/modules/OathLock.ts
@@ -2,20 +2,17 @@ import { buildModule } from "@nomicfoundation/hardhat-ignition/modules";
 
 const OathLockModule = buildModule("OathLockModule", (m) => {
   // Get current network from Hardhat Runtime Environment
-  const network = m.getParameter("network") as unknown as string;
-
-  //"flowMainnet": "0xF1815bd50389c46847f0Bda824eC8da914045D14",    // stgUSDC
-  //"zircuitMainnet": "0x3b952c8C9C44e8Fe201e2b26F6B2200203214cfF", // USDC.e
+  const network = m.getParameter("network", "localhost") as unknown as string;
 
   let usdcToken;
-  usdcToken = ""
-  if (network=="flowMainnet")    usdcToken = m.contractAt("IERC20", "0xF1815bd50389c46847f0Bda824eC8da914045D14");// stgUSDC
-  if (network=="zircuitMainnet") usdcToken = m.contractAt("IERC20", "0x3b952c8C9C44e8Fe201e2b26F6B2200203214cfF");// USDC.e
-  if (usdcToken == "") {
+
+  if (network === "flowMainnet") {
+    usdcToken = m.contractAt("IERC20", "0xF1815bd50389c46847f0Bda824eC8da914045D14"); // stgUSDC
+  } else if (network === "zircuitMainnet") {
+    usdcToken = m.contractAt("IERC20", "0x3b952c8C9C44e8Fe201e2b26F6B2200203214cfF"); // USDC.e
+  } else {
+    // If the network is not recognized, use a mock contract for testing purposes
     usdcToken = m.contract("MockUSDC", []);
-  }else {
-    // If the network is not recognized, we can still use a mock contract for testing purposes
-    usdcToken = m.contractAt("IERC20", usdcToken);
   }
 
   const oathLock = m.contract("OathLock", [usdcToken]);
