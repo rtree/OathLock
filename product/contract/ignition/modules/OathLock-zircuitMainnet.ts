1e4188c6a2ed8b2846b422bdd8b0be1dce550973
rtree <arakitomohiko@gmail.com>
2025-08-16 15:29:33 -0400

    ong

---
+++ product/contract/ignition/modules/OathLock-zircuitMainnet.ts
@@ -0,0 +1,24 @@
+import { buildModule } from "@nomicfoundation/hardhat-ignition/modules";
+
+const OathLockModule = buildModule("OathLockModule", (m) => {
+
+  let NETWORK = "zircuitMainnet";
+
+  let usdcToken;
+  switch(NETWORK) {
+    case "flowMainnet":
+      usdcToken = m.contractAt("IERC20", "0xF1815bd50389c46847f0Bda824eC8da914045D14"); // stgUSDC 
+      break;
+    case "zircuitMainnet":
+      usdcToken = m.contractAt("IERC20", "0x3b952c8C9C44e8Fe201e2b26F6B2200203214cfF"); // Zircuit Mainnet (USDC.e)
+      break;
+    default:
+      // For local or testnet networks, deploy a mock USDC contract
+      usdcToken = m.contract("MockUSDC", []);
+  }
+
+  const oathLock = m.contract("OathLock", [usdcToken]);
+  return { oathLock, usdcToken };
+});
+
+export default OathLockModule;
